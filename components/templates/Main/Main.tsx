53ee371c2bad6b5c6307432e3e747a5005e0e4b4
osmanemin <osmaneminusta@gmail.com>
2022-10-11 08:34:58 +0300

    use next image

--- components/templates/Main/Main.tsx
+++ components/templates/Main/Main.tsx
@@ -6,11 +6,13 @@ import NavigationMenu from "../../organisms/NavigationMenu";
 import UniAtlas from "../../molecules/UniAtlas";
 import Slogan from "../../molecules/Slogan";
 import UniFinder from "../../organisms/UniFinder";
+import Image from "next/image";
 
 export default function Main(): JSX.Element {
   return (
     <>
-      <div className={styles.bgImage}>
+      <Image src={"/background/pages/background.jpeg"} layout='fill' priority className={styles.bgImage}/>
+      <div className={styles.wrapper}>
         <UniAtlas className={styles.uniAtlas} />
         <Slogan />
         <NavigationMenu />
