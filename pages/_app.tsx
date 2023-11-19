d443a203ab5c1c2bef6f51bbec54ccb16b1a40e8
osmanemin <osmaneminusta@gmail.com>
2023-11-19 13:49:34 +0300

    add google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import type { AppProps } from "next/app";
 import "../styles/global.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-5R7EBQVFV6" />
+
+      <Script id="google-analytics">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5R7EBQVFV6');
+  `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
